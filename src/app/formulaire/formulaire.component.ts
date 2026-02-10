386cb5fe668b11b01a18626d7bb3cdecc0a51728
safa A <akdiid.safa@gmail.com>
2026-02-10 11:44:16 +0100

    feat: add imports

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './formulaire.component.html',
   styleUrl: './formulaire.component.css'
 })
